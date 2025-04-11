1f38f710ff53da23fadd70e02ea9eee781807960
Durga Bhavani <bhavanidurga6416@gmail.com>
2025-04-11 18:34:59 +0530

    Added GeoJSON version of urban areas shapefile

--- urban_rural.py
+++ urban_rural.py
@@ -1,9 +1,11 @@
 import pandas as pd
 import geopandas as gpd
 
-def classify_urban_rural(df, urban_shapefile=r'C:\Users\Bhavani\Desktop\Mobility\urban_areas.shp'):
+def classify_urban_rural(df, urban_shapefile = "urban_areas.shp"):
+
     gdf = gpd.GeoDataFrame(df, geometry=gpd.points_from_xy(df.longitude, df.latitude), crs="EPSG:4326")
-    urban_areas = gpd.read_file(urban_shapefile)
+    urban_areas = gpd.read_file("urban_areas.geojson")
+
     
     df['is_urban'] = gdf.geometry.within(urban_areas.unary_union)
     urban_stats = df[df['is_urban']].groupby('user_id').size()
